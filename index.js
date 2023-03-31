bb3e1e465d1ec5a86f0ae3bc6b0cebb81856c12b
Alina Rosa <rosalina@palitechnika.com>
2023-03-31 23:14:15 +0200

    color change on refresh

--- index.js
+++ index.js
@@ -14,30 +14,33 @@ function getHSLAElement(hsl, a) {
     return `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%, ${a})`
 }
 
-const baseColor = getColorValues()
-const suppColor = {h: baseColor.h, s: baseColor.s + 10, l: baseColor.l + 25}
-const shadowLColor = {h: baseColor.h, s: baseColor.s, l: baseColor.l + 10}
-const shadowDColor = {h: baseColor.h, s: baseColor.s, l: baseColor.l - 70}
+function setColors() {
+    const baseColor = getColorValues()
+    const suppColor = {h: baseColor.h, s: baseColor.s + 10, l: baseColor.l + 25}
+    const shadowLColor = {h: baseColor.h, s: baseColor.s, l: baseColor.l + 10}
+    const shadowDColor = {h: baseColor.h, s: baseColor.s, l: baseColor.l - 70}
+    
+    const bgColor = getHSLElement(baseColor)
+    const bgColor2 = getHSLElement(suppColor)
+    const shadowLight = getHSLAElement(shadowLColor, 0.45)
+    const shadowDark = getHSLAElement(shadowDColor, 0.3);
 
-const bgColor = getHSLElement(baseColor)
-const bgColor2 = getHSLElement(suppColor)
-const shadowLight = getHSLAElement(shadowLColor, 0.45)
-const shadowDark = getHSLAElement(shadowDColor, 0.3);
+    const r = document.querySelector(":root");
+
+    r.style.setProperty("--bgColor", bgColor);
+    r.style.setProperty("--bgColor2", bgColor2);
+    r.style.setProperty("--shadowLight", shadowLight);
+    r.style.setProperty("--shadowDark", shadowDark);
+}
 
 function getUstawaId() {
     return Math.floor(Math.random() * 26).toString();
 }
 
-const r = document.querySelector(":root");
-const title = document.querySelector(".title");
-const description = document.querySelector(".text");
-
-r.style.setProperty("--bgColor", bgColor);
-r.style.setProperty("--bgColor2", bgColor2);
-r.style.setProperty("--shadowLight", shadowLight);
-r.style.setProperty("--shadowDark", shadowDark);
-
 function refreshUstawa() {
+    const title = document.querySelector(".title");
+    const description = document.querySelector(".text");
+
     fetch("http://local.palitechnika.com:3000/ustawa")
     .then((response) => {
         return response.json();
@@ -52,5 +55,6 @@ function refreshUstawa() {
     });
 }
 
+setColors()
 refreshUstawa()
 
